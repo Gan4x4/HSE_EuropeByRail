e3b5d54849348e89c951f1de627b5857ec3113c7
Gan4x4 <gan4x4@yandex.ru>
2016-05-27 14:52:17 +0400

    Stage 3

--- sol/src/RailSystem.cpp
+++ sol/src/RailSystem.cpp
@@ -7,7 +7,12 @@ void RailSystem::reset(void) {
 
     // TODO: reset the data objects of the 
     // City objects' contained in cities
-    
+    for(auto p:  cities)
+    {
+         p.second->visited = false;
+         p.second->total_distance = INT_MAX;
+         p.second->total_fee = INT_MAX;
+    }
 }
 
 RailSystem::RailSystem(string const &filename) {
@@ -27,11 +32,10 @@ void RailSystem::load_services(string const &filename) {
 		inf >> from >> to >> fee >> distance;
 
 		if ( inf.good() ) {
-		
-			// TODO: Add entries in the cities container and
-			// and services in the rail system for the new 
-            // cities we read in.	
-
+            if (cities.find(from) == cities.end()){
+                cities[from] = new City();
+            }
+           outgoing_services[from].push_back(new Service(to,fee,distance)); 
 		}
 	}
 
@@ -42,7 +46,18 @@ RailSystem::~RailSystem(void) {
 
     // TODO: release all the City* and Service*
     // from cities and outgoing_services
-
+    for(auto p: outgoing_services)
+    {
+        for (Service* service: p.second)
+        {
+            delete service;
+        }
+    }
+    
+    for(auto p:  cities)
+    {
+        delete p.second;
+    }
 }
 
 void RailSystem::output_cheapest_route(const string& from,
@@ -70,8 +85,35 @@ pair<int, int> RailSystem::calc_route(string from, string to) {
     // You can use another container
 	priority_queue<City*, vector<City*>, Cheapest> candidates;
 
+    
+    City* current = cities[from]; 
+    current->total_distance = 0;
+    current->total_fee = 0;
+    //current->visited = true;
+    candidates.push(current);
+    do
+    {
+        current = candidates.top();
+        for (Service* service: outgoing_services[current->name]){
+            if (! cities[service->destination]->visited )
+            {
+                City * city =  cities[service->destination];
+                city->total_distance =  current->total_distance + service->distance;
+                city->total_fee =  current->total_distance + service->fee;
+                candidates.push(city);
+                // rebild
+            }
+        }
+        
+        current->visited = true;
+        candidates.pop();
+    }  while(candidates.size() > 0);
+    
     // TODO: Implement Dijkstra's shortest path algorithm to
     // find the cheapest route between the cities
+    
+    
+    
     
 
     // Return the total fee and total distance.
